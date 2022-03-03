b4ff50d6f8901bcc9ad47eff458ba00b0f52566a
p0tt3r-iOS <france998811@gmail.com>
2022-03-03 21:58:53 +0900

    Norm

--- img.c
+++ img.c
@@ -6,7 +6,7 @@
 /*   By: dha <dha@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 21:08:04 by dha               #+#    #+#             */
-/*   Updated: 2022/03/03 21:49:36 by dha              ###   ########seoul.kr  */
+/*   Updated: 2022/03/03 21:58:35 by dha              ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,28 @@
 void	init_img(t_game	*game)
 {
 	t_coord	coord;
-	
+	t_coord	tmp;
+
 	game->mlx = mlx_init();
-	game->win = mlx_new_window(game->mlx, game->width, game->height, 
+	game->win = mlx_new_window(game->mlx, game->width, game->height,
 			"so_long");
 	game->scr.img = mlx_new_image(game->mlx, game->width, game->height);
-	game->scr.addr = (int *) mlx_get_data_addr(game->scr.img, &game->scr.
-			bits_per_pixel, &game->scr.line_length, &game->scr.endian);
+	game->scr.addr = (int *) mlx_get_data_addr(game->scr.img, &game->\
+			scr.bits_per_pixel, &game->scr.line_length, &game->scr.endian);
 	coord.y = -1;
 	while (++coord.y < game->height)
 	{
 		coord.x = -1;
 		while (++coord.x < game->width)
-			game->scr.addr[coord.y * game->scr.line_length / 4 + coord.x] =
-					SCR_COLOR;
+			game->scr.addr[coord.y * game->scr.line_length / 4 + coord.x] \
+					= SCR_COLOR;
 	}
-	mlx_put_image_to_window(game->mlx, game->win, game->scr.img,0, 0);
-	
+	game->wall.img = mlx_xpm_file_to_image(game->mlx, \
+			"wall.xpm", tmp.x, tmp.y);
+	game->character.img = mlx_xpm_file_to_image(game->mlx, \
+			"character.xpm", tmp.x, tmp.y);
+	game->exit.img = mlx_xpm_file_to_image(game->mlx, \
+			"exit.xpm", tmp.x, tmp.y);
+	game->collectible.img = mlx_xpm_file_to_image(game->mlx, \
+			"collectible.xpm", tmp.x, tmp.y);
 }
